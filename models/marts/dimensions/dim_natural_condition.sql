83b5e6025259e65861b072b2006fd0a3abca4810
Phúc Trần <10a1tranhongphuc2021@gmail.com>
2026-09-13 21:03:12 +0700

    refactor: centralize favorable condition logic for BQ3

--- models/marts/dimensions/dim_natural_condition.sql
+++ models/marts/dimensions/dim_natural_condition.sql
@@ -9,8 +9,17 @@ WITH unique_conditions AS (
 )
 
 SELECT
-    ROW_NUMBER() OVER (ORDER BY weather_condition) AS natural_id,
+    ROW_NUMBER() OVER (
+        ORDER BY weather_condition, lighting_condition, roadway_surface_cond
+    ) AS natural_id,
     weather_condition,
     lighting_condition,
-    roadway_surface_cond
+    roadway_surface_cond,
+    CASE
+        WHEN weather_condition IN ('UNKNOWN', 'OTHER') THEN NULL
+        WHEN weather_condition IN ('CLEAR', 'CLOUDY/OVERCAST')
+         AND lighting_condition IN ('DAWN', 'DAYLIGHT', 'DARKNESS, LIGHTED ROAD')
+            THEN TRUE
+        ELSE FALSE
+    END AS is_favorable_condition
 FROM unique_conditions
